9b253011826ef0aa830cc216fad40e64d58789c5
rsyjswy23 <summerha0211@gmail.com>
2022-08-04 23:06:30 -0700

    set up env

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/project/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.microservices.project.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootConfiguration
+public class TwitterToKafkaServiceApplicationTests {
+    @Test
+    public void contextLoad() {
+
+    }
+}
