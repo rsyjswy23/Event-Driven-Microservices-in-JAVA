df0f6c4ad51af1854cf82cb82df1fba85c67a698
rsyjswy23 <summerha0211@gmail.com>
2022-08-05 21:45:35 -0700

    Added mock twitter stream and set it as default to avoid exceeding limit using twitter API

--- twitter-to-kafka-service/src/main/java/com/microservices/project/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/project/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
@@ -3,6 +3,7 @@ package com.microservices.project.twitter.to.kafka.service.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Data
@@ -11,4 +12,8 @@ import java.util.List;
 public class TwitterToKafkaServiceConfigData {
     private List<String> twitterKeywords;
     private String welcomeMessage;
+    private Boolean enableMockTweets;
+    private Long mockSleepMs;
+    private Integer mockMinTweetLength;
+    private Integer mockMaxTweetLength;
 }
